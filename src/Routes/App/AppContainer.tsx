aea8745fd0d3f8c1e2ae79bddc320e6ca75a9f7e
jerrynim <tjerry3@naver.com>
2019-03-03 21:03:42 +0900

    Drawer with react-naitive-draweR

--- src/Routes/App/AppContainer.tsx
+++ src/Routes/App/AppContainer.tsx
@@ -3,22 +3,20 @@ import {
   createStackNavigator,
   NavigationScreenProp,
   NavigationScreenProps,
-  createDrawerNavigator
+  createAppContainer
 } from "react-navigation";
-import {
-  SafeAreaView,
-  View,
-  Text,
-  Button,
-  Dimensions,
-  TouchableOpacity
-} from "react-native";
+import { SafeAreaView, View, Text, Button } from "react-native";
+import Drawer from "react-native-drawer";
 
 interface IProps {
   navigation: NavigationScreenProp<any, any>;
 }
 
-class HomeScreen extends React.Component {
+interface IState {
+  isMenuOpen: boolean;
+}
+
+class HomeScreen extends React.Component<IProps, IState> {
   static navigationOptions = (navigation: NavigationScreenProps) => {
     return {
       headerTitle: (
@@ -36,7 +34,7 @@ class HomeScreen extends React.Component {
       headerLeft: (
         <Button
           title={"Menu"}
-          onPress={() => navigation.navigation.toggleDrawer()}
+          onPress={() => console.log("let's change AppPresenter's isMenuOpen")}
         />
       ),
       headerRight: (
@@ -48,16 +46,47 @@ class HomeScreen extends React.Component {
     };
   };
 
+  // componentDidMount() {
+  //   this.props.navigation.setParams({ toggleMenu: this._toggleMenu });
+  // }
+
+  // _toggleMenu = () => {
+  //   this.setState({ isMenuOpen: !this.state.isMenuOpen });
+  // };
+
   render() {
     return (
-      <SafeAreaView>
-        <View>
-          <Text>This is home</Text>
-        </View>
-      </SafeAreaView>
+      <View
+        style={{
+          flex: 1,
+          alignItems: "center",
+          justifyContent: "center",
+          backgroundColor: ""
+        }}
+      >
+        <Text>Home Screen</Text>
+      </View>
     );
   }
 }
+
+const drawerStyles = {
+  drawer: {
+    shadowColor: "#000000",
+    shadowOpacity: 0.8,
+    shadowRadius: 3,
+    backgroundColor: "blue"
+  },
+  main: { paddingLeft: 3 }
+};
+
+const ControlPanel = () => {
+  return (
+    <View>
+      <Text>hi</Text>
+    </View>
+  );
+};
 
 class DiscoverScreen extends React.Component<IProps> {
   render() {
@@ -93,46 +122,34 @@ class SearchScreen extends React.Component<IProps> {
 
 const HomeStack = createStackNavigator({
   Home: {
-    screen: HomeScreen,
-    navigationOptions: ({ navigation }: NavigationScreenProps) => ({
-      title: "Home",
-      headerLeft: (
-        <TouchableOpacity onPress={() => navigation.navigate("DrawerOpen")}>
-          <Text>aaa</Text>
-        </TouchableOpacity>
-      )
-    })
+    screen: HomeScreen
   },
   Discover: {
-    screen: DiscoverScreen,
-    navigationOptions: () => ({
-      title: "Discover"
-    })
+    screen: DiscoverScreen
   },
   Search: { screen: SearchScreen }
 });
 
-const Drawer: React.SFC<IProps> = () => (
-  <View>
-    <Text>hi</Text>
-  </View>
-);
+const AppPresenter = createAppContainer(HomeStack);
 
-const AppContainer = createDrawerNavigator(
-  {
-    Home: {
-      screen: HomeStack
-    }
-  },
-  {
-    initialRouteName: "Home",
-    contentComponent: Drawer,
-    drawerWidth: Dimensions.get("window").width - 120
-  }
-);
-
-export default class App extends React.Component {
+class AppContainer extends React.Component<IState> {
+  public state = {
+    isMenuOpen: false
+  };
   render() {
-    return <AppContainer />;
+    const { isMenuOpen } = this.state;
+    return (
+      <Drawer
+        type="overlay"
+        open={isMenuOpen}
+        content={<ControlPanel />}
+        styles={drawerStyles}
+        openDrawerOffset={100}
+      >
+        <AppPresenter />
+      </Drawer>
+    );
   }
 }
+
+export default AppContainer;
