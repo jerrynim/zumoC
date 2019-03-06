8bce522f5b7c98e94ed0a6e0f4ca99b3f0e59277
jerrynim <tjerry3@naver.com>
2019-03-06 21:29:16 +0900

    Finishing Weaher

--- src/Routes/Home/HomeScreen.tsx
+++ src/Routes/Home/HomeScreen.tsx
@@ -5,6 +5,7 @@ import styled from "styled-components/native";
 import { WEATHERAPI_KEY } from "../../keys";
 import { reverseGeoCode } from "../../utils";
 import moment from "moment";
+import Icon from "react-native-vector-icons/Ionicons";
 
 const SCREEN_WIDTH = Dimensions.get("window").width;
 
@@ -12,9 +13,9 @@ const Weather = styled.View`
   display: flex;
   flex-direction: column;
   justify-content: center;
-  margin-top: 20px;
-  margin-bottom: 50px;
+  padding-top: 20px;
   text-align: center;
+  background-color: " rgba(0,0,0,0.07)";
 `;
 
 interface IProps {
@@ -45,7 +46,7 @@ class HomeScreen extends React.Component<IProps, IState> {
     temperature: 0,
     weather: "",
     GeoName: "",
-    WeekSchedule: []
+    WeekSchedule: [{ id: 0, day: "", dayName: "", temp: 0, weather: "" }]
   };
   // componentDidMount() {
   //   this.props.navigation.setParams({ toggleMenu: this._toggleMenu });
@@ -118,7 +119,6 @@ class HomeScreen extends React.Component<IProps, IState> {
     this.setState({
       WeekSchedule: WeekScheduleIN
     });
-    console.log(this.state.WeekSchedule);
   };
 
   getWeather = async (lat: number, lng: number) => {
@@ -135,7 +135,7 @@ class HomeScreen extends React.Component<IProps, IState> {
   };
 
   render() {
-    const { Date, Week } = this.state;
+    const { Date, Week, WeekSchedule } = this.state;
     return (
       <SafeAreaView
         forceInset={{
@@ -145,15 +145,78 @@ class HomeScreen extends React.Component<IProps, IState> {
         <Weather>
           <Text
             style={{
-              color: "black",
-              height: 30,
-              fontSize: 20,
+              color: "#c0392b",
+              fontSize: 25,
               textAlign: "center"
             }}
           >
             {Date} - {Week}
           </Text>
-          <View style={{ width: SCREEN_WIDTH, height: 150 }} />
+          <Text
+            style={{
+              marginTop: 20,
+              color: "black",
+              fontSize: 12,
+              textAlign: "center"
+            }}
+          >
+            이 주 의 Z U M O 가 이 드
+          </Text>
+          <View
+            style={{
+              width: SCREEN_WIDTH,
+              height: 100,
+              paddingLeft: 20,
+              paddingRight: 20,
+              paddingBottom: 30,
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignItems: "center"
+            }}
+          >
+            {WeekSchedule.map((schedule) => {
+              if (schedule.weather === "Rain") {
+                return (
+                  <View style={{ alignItems: "center" }} key={schedule.id}>
+                    <Text style={{ color: "#192a56" }}>
+                      {schedule.dayName}
+                    </Text>
+                    <Icon name={"ios-rainy"} size={20} />
+                    <Text>{schedule.temp}</Text>
+                  </View>
+                );
+              } else if (schedule.weather === "Clear") {
+                return (
+                  <View style={{ alignItems: "center" }} key={schedule.id}>
+                    <Text style={{ color: "#192a56" }}>
+                      {schedule.dayName}
+                    </Text>
+                    <Icon name={"md-sunny"} size={20} />
+                    <Text>{schedule.temp}</Text>
+                  </View>
+                );
+              } else if (schedule.weather === "Clouds") {
+                return (
+                  <View style={{ alignItems: "center" }} key={schedule.id}>
+                    <Text style={{ color: "#192a56" }}>
+                      {schedule.dayName}
+                    </Text>
+                    <Icon name={"ios-cloud-outline"} size={20} />
+                    <Text>{schedule.temp}</Text>
+                  </View>
+                );
+              } else {
+                return (
+                  <View style={{ alignItems: "center" }} key={schedule.id}>
+                    <Text style={{ color: "#192a56" }}>
+                      {schedule.dayName}
+                    </Text>
+                    <Text>{schedule.temp}</Text>
+                  </View>
+                );
+              }
+            })}
+          </View>
         </Weather>
         <Animated.ScrollView
           scrollEventThrottle={16}
