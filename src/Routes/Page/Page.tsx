6099ab11dc14cb32d87e109e3f497ecd62e61218
jerrynim <tjerry3@naver.com>
2019-03-21 11:06:12 +0900

    digging

--- src/Routes/Page/Page.tsx
+++ src/Routes/Page/Page.tsx
@@ -5,7 +5,8 @@ import {
   Image,
   ImageBackground,
   Dimensions,
-  ScrollView
+  ScrollView,
+  TouchableOpacity
 } from "react-native";
 import styled from "styled-components/native";
 import Card from "../../components/Card";
@@ -58,7 +59,34 @@ const GreyBar = styled.View`
   background-color: #f1f2f6;
 `;
 
-const Tags = styled.View``;
+const Tags = styled.View`
+  width: ${SCREEN_WIDTH};
+  height: 150;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+const TagText = styled.Text`
+  font-size: 15px;
+  color: #3867d6;
+  font-weight: 800;
+  margin-bottom: 10px;
+`;
+
+const TagBoxWrapper = styled.View`
+  width: ${SCREEN_WIDTH - 40};
+  margin-top: 10px;
+  margin-left: 20px;
+  margin-right: 20px;
+  flex-direction: row;
+  justify-content: center;
+`;
+
+const TagBox = styled.Text`
+  background-color: rgba(0, 0, 0, 0.05);
+  font-size: 15px;
+  margin-right: 10px;
+`;
 
 class Page extends React.Component {
   public render() {
@@ -74,10 +102,12 @@ class Page extends React.Component {
             style={{ width: SCREEN_WIDTH, height: 320 }}
           >
             <Header>
-              <Image
-                source={require("../../images/back.png")}
-                style={{ width: 23, height: 23 }}
-              />
+              <TouchableOpacity>
+                <Image
+                  source={require("../../images/back.png")}
+                  style={{ width: 23, height: 23 }}
+                />
+              </TouchableOpacity>
               <View style={{ justifyContent: "center", flexDirection: "row" }}>
                 <Image
                   source={require("../../images/share.png")}
@@ -117,7 +147,21 @@ class Page extends React.Component {
             />
           </SocialIcons>
           <GreyBar />
-          <Tags />
+          <Tags>
+            <TagText>TAG</TagText>
+            <TagBoxWrapper>
+              <TagBox>#카페</TagBox>
+              <TagBox>#핫플</TagBox>
+              <TagBox>#가로수길</TagBox>
+              <TagBox>#연남동</TagBox>
+              <TagBox>#컬러</TagBox>
+            </TagBoxWrapper>
+            <TagBoxWrapper>
+              <TagBox>#컬러풀</TagBox>
+              <TagBox>#서울</TagBox>
+              <TagBox>#포토존</TagBox>
+            </TagBoxWrapper>
+          </Tags>
         </ScrollView>
       </SafeAreaView>
     );
--- src/Routes/Search/SearchScreen.tsx
+++ src/Routes/Search/SearchScreen.tsx
@@ -1,24 +1,248 @@
-import React from "react";
-import { NavigationScreenProp } from "react-navigation";
-import { SafeAreaView, View, Button, Text } from "react-native";
+import React, { createRef } from "react";
+import {
+  SafeAreaView,
+  StyleSheet,
+  Dimensions,
+  ScrollView,
+  TouchableWithoutFeedback,
+  Animated,
+  Image,
+  Text,
+  View
+} from "react-native";
 
-interface IProps {
-  navigation: NavigationScreenProp<any, any>;
+const SCREEN_WIDTH = Dimensions.get("window").width;
+const SCREEN_HEIGHT = Dimensions.get("window").height;
+const images = [
+  { id: 1, src: require("../../images/valley.jpeg") },
+  { id: 2, src: require("../../images/lake.jpeg") },
+  { id: 3, src: require("../../images/forest.jpeg") },
+  { id: 4, src: require("../../images/flower.jpeg") }
+];
+
+interface IState {
+  activeImage: any;
 }
+class SearchScreen extends React.Component<IState> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeImage: null
+    };
+  }
+  public allImages = { images };
+  public oldPosition = { x: 0, y: 0, width: 0, height: 0 };
+  public position = new Animated.ValueXY();
+  public dimensions = new Animated.ValueXY();
+  public animation = new Animated.Value(0);
+  public activeImageStyle = null;
+  public viewImage = createRef();
+  openImage = (index) => {
+    this.allImages[index].measure((x, y, width, height, pageX, pageY) => {
+      this.oldPosition.x = pageX;
+      this.oldPosition.y = pageY;
+      this.oldPosition.width = width;
+      this.oldPosition.height = height;
 
-class SearchScreen extends React.Component<IProps> {
+      this.position.setValue({
+        x: pageX,
+        y: pageY
+      });
+
+      this.dimensions.setValue({
+        x: width,
+        y: height
+      });
+
+      console.log(pageX, pageY, width, height);
+
+      this.setState(
+        {
+          activeImage: images[index]
+        },
+        () => {
+          this.viewImage.measure((dx, dy, dWidth, dHeight, dPageX, dPageY) => {
+            console.log(dx, dy, dWidth, dHeight, dPageX, dPageY);
+
+            Animated.parallel([
+              Animated.timing(this.position.x, {
+                toValue: dPageX,
+                duration: 300
+              }),
+              Animated.timing(this.position.y, {
+                toValue: dPageY,
+                duration: 300
+              }),
+              Animated.timing(this.dimensions.x, {
+                toValue: dWidth,
+                duration: 300
+              }),
+              Animated.timing(this.dimensions.y, {
+                toValue: dHeight,
+                duration: 300
+              }),
+              Animated.timing(this.animation, {
+                toValue: 1,
+                duration: 300
+              })
+            ]).start();
+          });
+        }
+      );
+    });
+  };
+  closeImage = () => {
+    Animated.parallel([
+      Animated.timing(this.position.x, {
+        toValue: this.oldPosition.x,
+        duration: 300
+      }),
+      Animated.timing(this.position.y, {
+        toValue: this.oldPosition.y,
+        duration: 250
+      }),
+      Animated.timing(this.dimensions.x, {
+        toValue: this.oldPosition.width,
+        duration: 250
+      }),
+      Animated.timing(this.dimensions.y, {
+        toValue: this.oldPosition.height,
+        duration: 250
+      }),
+      Animated.timing(this.animation, {
+        toValue: 0,
+        duration: 250
+      })
+    ]).start(() => {
+      this.setState({
+        activeImage: null
+      });
+    });
+  };
   render() {
+    const activeImageStyle = {
+      width: this.dimensions.x,
+      height: this.dimensions.y,
+      left: this.position.x,
+      top: this.position.y
+    };
+
+    const animatedContentY = this.animation.interpolate({
+      inputRange: [0, 1],
+      outputRange: [-150, 0]
+    });
+
+    const animatedContentOpacity = this.animation.interpolate({
+      inputRange: [0, 0.5, 1],
+      outputRange: [0, 1, 1]
+    });
+
+    const animatedContentStyle = {
+      opacity: animatedContentOpacity,
+      transform: [
+        {
+          translateY: animatedContentY
+        }
+      ]
+    };
+
+    const animatedCrossOpacity = {
+      opacity: this.animation
+    };
+
     return (
-      <SafeAreaView>
-        <View>
-          <Text>this is Search</Text>
+      <SafeAreaView style={{ flex: 1 }}>
+        <ScrollView style={{ flex: 1 }}>
+          {images.map((image, index) => {
+            return (
+              <TouchableWithoutFeedback
+                onPress={() => this.openImage(index)}
+                key={image.id}
+              >
+                <Animated.View
+                  style={{
+                    height: SCREEN_HEIGHT - 150,
+                    width: SCREEN_WIDTH,
+                    padding: 15
+                  }}
+                >
+                  <Image
+                    ref={(image) => (this.allImages[index] = image)}
+                    source={image.src}
+                    style={{
+                      flex: 1,
+                      resizeMode: "cover",
+                      borderRadius: 20
+                    }}
+                  />
+                </Animated.View>
+              </TouchableWithoutFeedback>
+            );
+          })}
+        </ScrollView>
+        <View
+          style={StyleSheet.absoluteFill}
+          pointerEvents={this.state.activeImage ? "auto" : "none"}
+        >
+          <View
+            style={{ flex: 2, zIndex: 1001 }}
+            ref={(view) => (this.viewImage = view)}
+          >
+            <Animated.Image
+              source={
+                this.state.activeImage ? this.state.activeImage.src : null
+              }
+              style={[
+                {
+                  resizeMode: "cover",
+                  top: 0,
+                  left: 0,
+                  height: null,
+                  width: null
+                },
+                activeImageStyle
+              ]}
+            />
+            <TouchableWithoutFeedback onPress={() => this.closeImage()}>
+              <Animated.View
+                style={[
+                  { position: "absolute", top: 30, right: 30 },
+                  animatedCrossOpacity
+                ]}
+              >
+                <Text
+                  style={{ fontSize: 24, fontWeight: "bold", color: "white" }}
+                >
+                  X
+                </Text>
+              </Animated.View>
+            </TouchableWithoutFeedback>
+          </View>
+          <Animated.View
+            style={[
+              {
+                flex: 1,
+                zIndex: 1000,
+                backgroundColor: "white",
+                padding: 20,
+                paddingTop: 50
+              },
+              animatedContentStyle
+            ]}
+          >
+            <Text style={{ fontSize: 24, paddingBottom: 10 }}>
+              Unsure Programmer
+            </Text>
+            <Text>
+              Eiusmod consectetur cupidatat dolor Lorem excepteur excepteur.
+              Nostrud sint officia consectetur eu pariatur laboris est velit.
+              Laborum non cupidatat qui ut sit dolore proident.
+            </Text>
+          </Animated.View>
         </View>
-        <Button
-          title={"go to Home"}
-          onPress={() => this.props.navigation.navigate("Home")}
-        />
       </SafeAreaView>
     );
   }
 }
+
 export default SearchScreen;
